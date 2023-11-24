be9811cd7b9a29d2f94c80d3d062506c4505fcd8
Mohammad Rahman <me@shadikur.com>
2023-11-24 12:36:54 +0100

    Updated tsconfig - root directory

---
+++ dist/app.js
@@ -0,0 +1,18 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const app = (0, express_1.default)();
+// Middlewares
+app.use(express_1.default.json());
+app.use((0, cors_1.default)());
+// All routes
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Welcome to User Management API',
+    });
+});
+exports.default = app;
