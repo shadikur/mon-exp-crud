3d662eae9c8a105ef750a7c0f86f2f341a94a580
Mohammad Rahman <me@shadikur.com>
2023-11-25 15:38:07 +0100

    Zod validation added

---
+++ src/app/modules/users/users.zod.ts
@@ -0,0 +1,35 @@
+import { z } from 'zod';
+//import { FullName, Address, Order, User } from './users.interface';
+
+export const FullNameSchema = z.object({
+  firstName: z.string(),
+  lastName: z.string(),
+});
+
+export const AddressSchema = z.object({
+  street: z.string(),
+  city: z.string(),
+  country: z.string(),
+});
+
+export const OrderSchema = z.object({
+  productName: z.string(),
+  price: z.number(),
+  quantity: z.number(),
+});
+
+export const UserSchema = z.object({
+  userId: z.number(),
+  username: z.string(),
+  password: z.string(),
+  fullName: FullNameSchema,
+  age: z.number(),
+  email: z.string().email(),
+  isActive: z.boolean(),
+  hobbies: z.array(z.string()),
+  address: AddressSchema,
+  orders: z.array(OrderSchema),
+  gender: z.enum(['Male', 'Female', 'Other']),
+});
+
+export default UserSchema;
